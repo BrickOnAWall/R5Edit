ff41fb216df86f3ab3bf6c296646ac03ed06e05a
BrickOnAWall <89106240+BrickOnAWall@users.noreply.github.com>
2021-11-28 12:09:17 -0600

    Update RUN ME.bat

--- src/RUN ME.bat
+++ src/RUN ME.bat
@@ -1,2 +1,11 @@
+@echo off
+echo                        Thanks to fireproof for making all the other base files needed. 
+echo                             This is for making your life 1000x easier at saving!
+echo                            for this to work you need to put your console logs
+echo                      which you have copied from the console into the save file in /examples
+echo                                     proceed once you have done that!
+pause
+cls
+echo.
 python main.py ../examples/save.txt ../examples/output.nut
 pause
