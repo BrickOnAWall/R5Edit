ccc294df76f27c8e4a161d5f6b329c7180b0b6a1
BrickOnAWall <89106240+BrickOnAWall@users.noreply.github.com>
2021-12-02 19:50:10 -0600

    Updated Some Files

--- src/RUN ME.bat
+++ src/RUN ME.bat
@@ -7,5 +7,5 @@ echo                                     proceed once you have done that!
 pause
 cls
 echo.
-python main.py ../examples/save.txt ../examples/output.nut
+python main.py ../saves/save.txt ../saves/output.nut
 pause
